15c225918286ecae3a00f3ed58481481e9faeb84
sena <hargiantofebrisena@gmail.com>
2022-04-05 15:46:16 +0700

    initial

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-let connectionString = `postgres://svugkbjyggpdpe:6a09f9acae4e3f2ae2ae27780ba186458b902e564d41044d8cb688cf04e5e9b0@ec2-54-173-77-184.compute-1.amazonaws.com:5432/dea5nj9us5cpib`;
+let connectionString = `postgres://hnnfkiktpzylib:9e8ce87c2415d15017121c4ec485c9fbd1d5091d85609a524b88113854cec8bd@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d807kji38sj9ba`;
 
 const dbPool = new Pool({
   connectionString: connectionString,
